0f948037d504287d0b8481c2177d57d5f84512eb
Attila <zsigmond.attila.laszlo@students.jedlik.eu>
2025-02-18 21:08:14 +0100

    encapsulation majdnem kész

--- VideoJatekSim/VideoJatekSim/Program.cs
+++ VideoJatekSim/VideoJatekSim/Program.cs
@@ -8,103 +8,25 @@ namespace VideoJatekSim
         static void Main(string[] args)
         {
             Jatekos Herny = new Jatekos() { Eletero = 100, Tamadas = 50, Penc = 10, Poz = 3, PozSzam = 1, Karizma = 0, Vege = false, Gyozott = false };
-            Ellenseg Makvratr = new Ellenseg() { Eletero = 400, Tamadas = 200, Fokozat = 5 };
-            Ellenseg Reszeg_Paraszt_Bottal = new Ellenseg() { Eletero = 70, Tamadas = 30, Fokozat = 1 };
-            Ellenseg Paraszt = new Ellenseg() { Eletero = 100, Tamadas = 40, Fokozat = 2 };
-            Ellenseg Alap_Bandita = new Ellenseg() { Eletero = 150, Tamadas = 110, Fokozat = 3 };
-            Ellenseg Magasan_Kepzett_Bandita = new Ellenseg() { Eletero = 190, Tamadas = 160, Fokozat = 4 };
             int markvratr_poz = 713;
             Palya Telepules = new Palya();
             int lepes_szam = 0;
             int linewidth = 60;
             Random rand = new Random();
-            int[] terkep =
-            {
-                1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-                1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-                1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,1,1,1,1,1,
-                1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,1,2,1,1,1,
-                1,1,1,1,1,1,1,2,2,2,2,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,1,1,1,
-                1,1,1,1,1,1,1,2,2,2,2,1,1,1,1,1,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-                1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-                1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-                1,1,1,1,0,1,1,1,0,0,1,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-                1,1,1,0,1,0,1,0,1,1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-                1,1,0,1,1,1,0,1,1,1,1,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,2,2,2,1,1,0,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,4,4,4,4,4,4,1,1,
-                0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,0,1,1,1,1,1,1,1,0,1,0,1,1,1,1,1,4,4,4,4,4,4,4,4,4,
-                1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,0,1,1,1,1,1,0,1,1,1,0,1,1,1,4,4,4,4,4,4,4,4,4,1,
-                1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,0,1,0,1,1,1,1,1,0,0,0,0,0,0,1,1,1,1,1,0,0,0,1,0,1,1,1,1,0,1,1,1,1,4,4,4,4,4,4,4,4,1,
-                1,1,1,1,1,1,3,3,3,3,3,3,3,3,1,1,1,0,1,1,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,0,1,1,1,1,1,4,4,4,4,4,1,1,
-                1,1,1,1,1,3,3,3,3,3,3,3,3,3,3,1,1,1,1,1,1,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,0,1,1,1,4,4,4,1,1,1,
-                1,1,1,1,1,3,3,3,3,3,3,3,3,3,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,0,0,1,1,1,1,1,1,0,
-                1,1,1,1,1,3,3,3,3,3,3,3,3,3,3,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,1,1,0,0,0,1,1,1,0,0,
-                1,1,1,1,1,3,3,3,3,3,3,3,3,3,3,1,1,1,1,1,1,1,1,1,1,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,1,1,1,0,0,0,0,0,0,
-                1,1,1,1,1,1,3,3,3,3,3,3,3,3,1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,0,0,0,0,
-                1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,
-
-
-            };
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.Write("Érted a játék írányítását");
+            Console.Write("Ez a projekt egy egyszerű rpg játékot szimulál ahol a kék a vizet jelzi, a zöld a mezőt \na vörös a falukat a szürke a várost és a sötétszürke a várat\n\nÉrted a játék írányítását");
             Console.ReadKey();
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.Write("Kezdődhet a játék");
+            Console.Write("IV Károly király halála után fia II Makvratr király egy erős és gazdag királyságot örökölt,de a vasököllel való \nuralkodása és a rossz gazdaságpolitikája rossz hatással volt az életminőségre,ezért egy nagyon hatásos \nvérnyomáscsökkentő éjszaka után elhatározod hogy ennek véget kell vetni és felébredsz egy mezőn\n\nKezdődhet a játék");
             Console.ReadKey();
             Console.Clear();
-
             while (Herny.Vege == false)
             {
-                Herny.PozSzam = terkep[Herny.Poz];
-                int index = 0;
-
-                foreach (int i in terkep)
+                Herny.PozSzam = Telepules.terkep[Herny.Poz];
+                var map = Telepules.GenerateMap(Herny.Poz, markvratr_poz, linewidth);
+                foreach (var (character, color) in map)
                 {
-                    if (Herny.Poz == index)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Yellow;
-                        Console.Write("█");
-                    }
-                    else if (markvratr_poz == index)
-                    {
-                        Console.ForegroundColor = ConsoleColor.DarkRed;
-                        Console.Write("█");
-                    }
-                    else
-                    {
-                        switch (i)
-                        {
-                            case 0:
-                                Console.ForegroundColor = ConsoleColor.Blue;
-                                Console.Write("█");
-                                break;
-                            case 1:
-                                Console.ForegroundColor = ConsoleColor.Green;
-                                Console.Write("█");
-                                break;
-                            case 2:
-                                Console.ForegroundColor = ConsoleColor.Red;
-                                Console.Write("█");
-                                break;
-                            case 3:
-                                Console.ForegroundColor = ConsoleColor.Gray;
-                                Console.Write("█");
-                                break;
-                            case 4:
-                                Console.ForegroundColor = ConsoleColor.DarkGray;
-                                Console.Write("█");
-                                break;
-                        }
-                    }
-                    index++;
-                    if (index % linewidth == 0)
-                    {
-                        Console.WriteLine("");
-                    }
-
+                    Console.ForegroundColor = color;
+                    Console.Write(character);
                 }
-                Console.WriteLine("");
                 if (Herny.Eletero > 100)
                 {
                     Console.ForegroundColor = ConsoleColor.Blue;
@@ -133,145 +55,30 @@ namespace VideoJatekSim
                 Console.ForegroundColor = ConsoleColor.White;
                 Console.Write("\tTámadás: " + Herny.Tamadas);
                 Console.ForegroundColor = ConsoleColor.DarkYellow;
-                Console.Write("\tPénz: " + Herny.Penc + " Gr");
+                Console.Write("\tPénz: " + Herny.Penc + " Gr\n\n");
+
                 Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine("");
-                Console.WriteLine("");
-                if (lepes_szam > 25 && Herny.PozSzam == 1)
-                {
-
-                    int szerencse = rand.Next(1, 101);
-                    if (szerencse <= 5)
-                    {
-                        Console.WriteLine("Ellenség támad rád");
-                        int ellenseg_szint = rand.Next(1, 15);
-                        if (ellenseg_szint <= 5)
-                        {
-                            Herny.Csata_feldolgozas(Reszeg_Paraszt_Bottal);
-                        }
-                        else if (ellenseg_szint <= 9)
-                        {
-
-                            Herny.Csata_feldolgozas(Paraszt);
-                        }
-                        else if (ellenseg_szint <= 12)
-                        {
-
-                            Herny.Csata_feldolgozas(Alap_Bandita);
-                        }
-                        else
-                        {
-                            Herny.Csata_feldolgozas(Magasan_Kepzett_Bandita);
-                        }
-                    }
-                }
-
-                if ((Herny.PozSzam == 2 || Herny.PozSzam == 3))
-                {
-                    switch (Telepulesimenu())
-                    {
-                        case ConsoleKey.S:
-                            break;
-                        case ConsoleKey.V:
-                            Console.WriteLine(Herny.Vasarlas(Telepules));
-                            break;
-                        case ConsoleKey.M:
-                            Console.WriteLine(Herny.Munka());
-
-                            break;
-                    }
-                }
-                Console.ForegroundColor = ConsoleColor.White;
-
-                switch (Menu())
-                {
-                    case ConsoleKey.W:
-                        if (Herny.Poz > 60 || terkep[Herny.Poz - 60] != 4 && Herny.Karizma > 6)
-                        {
-                            Herny.Poz -= 60;
-                        }
-                        else
-                        {
-                            break;
-                        }
-                        break;
-                    case ConsoleKey.D:
-                        if ((Herny.Poz + 1) % 60 != 0 || terkep[Herny.Poz + 1] != 4 && Herny.Karizma > 6)
-                        {
-                            Herny.Poz += 1;
-                        }
-                        else
-                        {
-                            break;
-                        }
-                        break;
-                    case ConsoleKey.S:
-                        if (Herny.Poz + 60 < terkep.Length || terkep[Herny.Poz + 60] != 4 && Herny.Karizma > 6)
-                        {
-                            Herny.Poz += 60;
-                        }
-                        else
-                        {
-                            break;
-                        }
-                        break;
-                    case ConsoleKey.A:
-                        if ((Herny.Poz) % 60 != 0 || terkep[Herny.Poz - 1] != 4 && Herny.Karizma > 6)
-                        {
-                            Herny.Poz -= 1;
-                        }
-                        else
-                        {
-                            break;
-                        }
-                        break;
-                }
-                if (Herny.Poz == markvratr_poz)
-                {
-                    if (Herny.Csata_feldolgozas(Makvratr) == "\nNyertél\n")
-                    {
-                        Herny.Gyozott = true;
-                        Herny.Vege = true;
-                    }
-                    if (Herny.PozSzam == 1)
-                    {
-                        lepes_szam++;
-                    }
-                }
-                    Console.Clear();
+                Herny.Talalkozas();
+                Console.Write( Herny.Telepulesimenuszoveg());
+                Herny.Megerkezes(Console.ReadKey().Key!);
+                Console.Write(Herny.MenuSzoveg());
+                Herny.Mozgas(Console.ReadKey().Key!);
+                Console.Clear();
+            if (Herny.Poz == markvratr_poz)
+                Herny.Vegso_csata();
+                lepes_szam++;
             }
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
             if(Herny.Gyozott == true)
             {
-                Console.WriteLine("");
+                Console.WriteLine("A hősi küldetésed sikerrel zárult, a király elhalálozása a gravticáció\n által nem lett rajtad felelősségre vonva, az hogy ki lesz az \núj király az már más gondja");
             }
             else
             {
-                Console.WriteLine("");
+                Console.WriteLine("A hősi küldetésed rövid és elég szomorú véget ért,na ne csüggedj\nhiszen így esélyt adtál arra hogy más vigye el a hírnevet.\n");
             }
             Console.ReadLine();
         }
-
-            static ConsoleKey Telepulesimenu()
-            {
-                Console.WriteLine("Akarsz-e valamit csinálni?");
-                Console.WriteLine("\tS: Semmit");
-                Console.WriteLine("\tV: Vásásrlás");
-                Console.WriteLine("\tM: Munka");
-                Console.Write("Mi a választásod? ");
-                return Console.ReadKey().Key!;
-            }
-
-            static ConsoleKey Menu()
-            {
-                Console.WriteLine("Melyik irányba haladjunk tovább?");
-                Console.WriteLine("\tW: fel");
-                Console.WriteLine("\tD: jobbra");
-                Console.WriteLine("\tS: lefelé");
-                Console.WriteLine("\tA: balra");
-                Console.Write("Merre? ");
-                return Console.ReadKey().Key!;
-            }
-
-        
     }
 }
--- VideoJatekSim/VideoJatekSim/jatekos.cs
+++ VideoJatekSim/VideoJatekSim/jatekos.cs
@@ -12,12 +12,19 @@ namespace VideoJatekSim
         public int Eletero { get; set; }
         public int Tamadas { get; set; }
         public int Poz { get; set; }
-        public int PozSzam {  get; set; }
+        public int PozSzam { get; set; }
         public int Penc { get; set; }
         public int Karizma { get; set; }
         public bool Vege { get; set; }
         public bool Gyozott { get; set; }
+        public int LepesSzam { get; set; }
+        Ellenseg Reszeg_Paraszt_Bottal = new Ellenseg() { Eletero = 70, Tamadas = 30, Fokozat = 1 };
+        Ellenseg Paraszt = new Ellenseg() { Eletero = 100, Tamadas = 40, Fokozat = 2 };
+        Ellenseg Alap_Bandita = new Ellenseg() { Eletero = 150, Tamadas = 110, Fokozat = 3 };
+        Ellenseg Magasan_Kepzett_Bandita = new Ellenseg() { Eletero = 190, Tamadas = 160, Fokozat = 4 };
+        Ellenseg Makvratr = new Ellenseg() { Eletero = 400, Tamadas = 200, Fokozat = 5 };
 
+        Palya Telepules = new Palya();
         public string Vasarlas(Palya Telepules)
         {
             if (PozSzam == 2)
@@ -26,14 +33,14 @@ namespace VideoJatekSim
                 Console.Write("Melyik tárgyat szeretnéd megvásárolni? (Az áru árát nem adjuk vissza) ");
                 int vasarolni_kivant_targy = Convert.ToInt32(Console.ReadLine());
                 Console.Clear();
-                if (Penc >= Telepules.falusi_piac[vasarolni_kivant_targy].Ar)
+                if (Penc >= Telepules.falusi_piac[vasarolni_kivant_targy -1].Ar)
                 {
                     Penc -= Telepules.falusi_piac[vasarolni_kivant_targy].Ar;
-                    if(Eletero + Telepules.falusi_piac[vasarolni_kivant_targy].Gyogyitas < 100)
+                    if (Eletero + Telepules.falusi_piac[vasarolni_kivant_targy].Gyogyitas < 100)
                     {
                         Eletero += Telepules.falusi_piac[vasarolni_kivant_targy].Gyogyitas;
                     }
-                    else if(Eletero < 100)
+                    else if (Eletero < 100)
                     {
                         Eletero = 100;
                     }
@@ -42,17 +49,17 @@ namespace VideoJatekSim
                         Eletero += 0;
                     }
                     if (Telepules.falusi_piac[vasarolni_kivant_targy].Vedelem > 0)
-                    Eletero = 100 + Telepules.falusi_piac[vasarolni_kivant_targy].Vedelem;
+                        Eletero = 100 + Telepules.falusi_piac[vasarolni_kivant_targy].Vedelem;
 
                     if (Telepules.falusi_piac[vasarolni_kivant_targy].Sebzes > 0)
-                    Tamadas = 50 + Telepules.falusi_piac[vasarolni_kivant_targy].Sebzes;
+                        Tamadas = 50 + Telepules.falusi_piac[vasarolni_kivant_targy].Sebzes;
 
                     if (Telepules.falusi_piac[vasarolni_kivant_targy].Karizmatikum > 0)
                         Karizma = 0 + Telepules.falusi_piac[vasarolni_kivant_targy].Karizmatikum;
                     return "\nsikeres vásárlás\n";
 
                 }
-                else 
+                else
                 {
                     return "\nnincs pénzed talán dolgozni kéne\n";
                 }
@@ -93,31 +100,31 @@ namespace VideoJatekSim
                     return "\nnincs pénzed talán dolgozni kéne\n";
                 }
             }
-            
+
         }
-        
+
         public string Munka()
+        {
+            if (PozSzam == 2)
             {
-                if (PozSzam == 2)
+                Penc += 50;
+                return "\nSikeres munka\n";
+            }
+            else
+            {
+                if (Karizma > 4)
                 {
-                    Penc += 50;
+                    Penc += 100;
                     return "\nSikeres munka\n";
                 }
                 else
                 {
-                    if (Karizma > 4)
-                    {
-                        Penc += 100;
-                        return "\nSikeres munka\n";
-                    }
-                    else
-                    {
-                        Penc += 0;
-                        return "\nNem néztél ki elég jól a jóembereknek\n";
-                    }
+                    Penc += 0;
+                    return "\nNem néztél ki elég jól a jóembereknek\n";
                 }
             }
-       public string Csata_feldolgozas(Ellenseg elleseg)
+        }
+        public string Csata_feldolgozas(Ellenseg elleseg)
         {
             elleseg.Csata(this);
             if (elleseg.Csata(this) == 0)
@@ -135,7 +142,149 @@ namespace VideoJatekSim
                 return "\nDöntetlen\n";
             }
         }
-
+        public string Talalkozas()
+        {
+            Random rand = new Random();
+            if (LepesSzam > 25 && PozSzam == 1)
+            {
+                int szerencse = rand.Next(1, 101);
+                if (szerencse <= 5)
+                {
+                    Console.WriteLine("Ellenség támad rád");
+                    int ellenseg_szint = rand.Next(1, 15);
+                    if (ellenseg_szint <= 5)
+                    {
+                        return Csata_feldolgozas(Reszeg_Paraszt_Bottal);
+                    }
+                    else if (ellenseg_szint <= 9)
+                    {
+                        return Csata_feldolgozas(Paraszt);
+                    }
+                    else if (ellenseg_szint <= 12)
+                    {
+                        return Csata_feldolgozas(Alap_Bandita);
+                    }
+                    else
+                    {
+                        return Csata_feldolgozas(Magasan_Kepzett_Bandita);
+                    }
+                }
+            }
+            return "\nNincs találkozás\n";
         }
-    }
+        public void Mozgas(ConsoleKey info)
+        {
+            switch (info)
+            {
+                case ConsoleKey.W:
+                    if (Poz > 60)
+                    {
+                        Poz -= 60;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                    break;
+                case ConsoleKey.D:
+                    if ((Poz + 1) % 60 != 0)
+                    {
+                        Poz += 1;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                    break;
+                case ConsoleKey.S:
+                    if (Poz + 60 < terkep.Length)
+                    {
+                        Poz += 60;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                    break;
+                case ConsoleKey.A:
+                    if ((Poz) % 60 != 0)
+                    {
+                        Poz -= 1;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                    break;
+            }
+        }
+        public string MenuSzoveg()
+        {
+            return "Melyik irányba haladjunk tovább?\n\tW: fel\n\tD: jobbra\n\tS: lefelé\n\tA: balra\nMerre? ";
+        }
+        int[] terkep =
+           {
+                1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+                1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+                1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,1,1,1,1,1,
+                1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,1,2,1,1,1,
+                1,1,1,1,1,1,1,2,2,2,2,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,1,1,1,
+                1,1,1,1,1,1,1,2,2,2,2,1,1,1,1,1,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+                1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+                1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+                1,1,1,1,0,1,1,1,0,0,1,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+                1,1,1,0,1,0,1,0,1,1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+                1,1,0,1,1,1,0,1,1,1,1,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,2,2,2,1,1,0,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,4,4,4,4,4,4,1,1,
+                0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,0,1,1,1,1,1,1,1,0,1,0,1,1,1,1,1,4,4,4,4,4,4,4,4,4,
+                1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,0,1,1,1,1,1,0,1,1,1,0,1,1,1,4,4,4,4,4,4,4,4,4,1,
+                1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,0,1,0,1,1,1,1,1,0,0,0,0,0,0,1,1,1,1,1,0,0,0,1,0,1,1,1,1,0,1,1,1,1,4,4,4,4,4,4,4,4,1,
+                1,1,1,1,1,1,3,3,3,3,3,3,3,3,1,1,1,0,1,1,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,0,1,1,1,1,1,4,4,4,4,4,1,1,
+                1,1,1,1,1,3,3,3,3,3,3,3,3,3,3,1,1,1,1,1,1,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,0,1,1,1,4,4,4,1,1,1,
+                1,1,1,1,1,3,3,3,3,3,3,3,3,3,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,0,0,1,1,1,1,1,1,0,
+                1,1,1,1,1,3,3,3,3,3,3,3,3,3,3,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,1,1,0,0,0,1,1,1,0,0,
+                1,1,1,1,1,3,3,3,3,3,3,3,3,3,3,1,1,1,1,1,1,1,1,1,1,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,1,1,1,0,0,0,0,0,0,
+                1,1,1,1,1,1,3,3,3,3,3,3,3,3,1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,0,0,0,0,
+                1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,
 
+
+            };
+        public string Megerkezes(ConsoleKey info)
+        {
+            if ((PozSzam == 2 || PozSzam == 3))
+            {
+                switch (info)
+                {
+                    case ConsoleKey.S:
+                        return "Semmit nem csináltál";
+                    case ConsoleKey.V:
+                        return Vasarlas(Telepules);
+                    case ConsoleKey.M:
+                        return Munka();
+                }
+            }
+            return "";
+        }
+        public string Telepulesimenuszoveg()
+        {
+            if(PozSzam == 3 || PozSzam == 2)
+                return "\nAkarsz-e valamit csinálni?\n\tS: Semmit\n\tV: Vásásrlás\n\tM: Munka\nMi a választásod?";
+            else
+            {
+                return "";
+            }
+        }
+        public void Vegso_csata()
+        {
+
+                Csata_feldolgozas(Makvratr);
+                if (Csata_feldolgozas(Makvratr) == "\nNyertél\n")
+                {
+                    Gyozott = true;
+                    Vege = true;
+                }
+                else if (Csata_feldolgozas(Makvratr) == "\nVesztettél\n")
+                    Vege = true;
+        }
+        
+    }
+}
